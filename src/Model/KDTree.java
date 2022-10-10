0a71f5f7d88bddd0614af365bab44668e08a03ec
Hai <hoanghai050605@gmail.com>
2022-10-10 20:56:33 +0800

    Updated

--- src/Model/KDTree.java
+++ src/Model/KDTree.java
@@ -1,5 +1,4 @@
 package Model;
-
 import java.util.*;
 
 public class KDTree<T> {
@@ -9,10 +8,10 @@ public class KDTree<T> {
     private double minDist = Double.MAX_VALUE;
     private PriorityQueue<CustomKDTreeNode<T>> nearestNeighbours = new PriorityQueue<>(
             10, (n1, n2) -> {
-                if (n1.getDist() < n2.getDist()) return 1;
-                if (n1.getDist() > n2.getDist()) return -1;
-                return 0;
-            });
+        if (n1.getDist() < n2.getDist()) return 1;
+        if (n1.getDist() > n2.getDist()) return -1;
+        return 0;
+    });
 
     public KDTree(int dimensions, double[][] points) {
         this.dimensions = dimensions;
@@ -73,6 +72,81 @@ public class KDTree<T> {
     }
 
     public CustomKDTreeNode<T> getRoot(){return root;}
+
+    public void insert(T item, double[] point) { root = insertRec(item, point, root, 0);}
+    private CustomKDTreeNode<T> insertRec(T item, double[] point, CustomKDTreeNode<T> root, int depth) {
+        int cd = depth % dimensions;
+        if(root == null) return new CustomKDTreeNode<>(item, 2 , point);
+        else if(point[cd] < root.getPoint()[cd])
+            root.customNeighbours[0] = insertRec(item, point, root.customNeighbours[0], depth+1);
+        else
+            root.customNeighbours[1] = insertRec(item, point, root.customNeighbours[1], depth+1);
+        return root;
+    }
+
+    private CustomKDTreeNode<T> minNode(CustomKDTreeNode<T> x, CustomKDTreeNode<T> y, CustomKDTreeNode<T> z, int d)
+    {
+        CustomKDTreeNode<T> res = x;
+        if (y != null && y.getPoint()[d] < res.getPoint()[d])
+            res = y;
+        if (z != null && z.getPoint()[d] < res.getPoint()[d])
+            res = z;
+        return res;
+    }
+    private CustomKDTreeNode<T> findMinRec(CustomKDTreeNode<T> root, int d, int depth) {
+        if (root == null) return null;
+        int cd = depth % dimensions;
+        if (cd == d) {
+            if (root.customNeighbours[0] == null)
+                return root;
+            return findMinRec(root.customNeighbours[0], d, depth+1);
+        }
+        return minNode(root,
+                findMinRec(root.customNeighbours[0], d, depth+1),
+                findMinRec(root.customNeighbours[1], d, depth+1), d);
+    }
+    private CustomKDTreeNode<T> findMin(CustomKDTreeNode<T> root, int d)
+    {
+        return findMinRec(root, d, 0);
+    }
+    public void delete(double[] point) {root = deleteRec(root, point, 0);}
+    private CustomKDTreeNode<T> deleteRec(CustomKDTreeNode<T> root, double[] point, int depth) {
+        if (root == null) return null;
+        int cd = depth % dimensions;
+        if (Arrays.equals(root.getPoint(), point)) {
+            if (root.customNeighbours[1] != null) {
+                // find p, the point with the minimum x value from the subtree rooted at the right child.
+                CustomKDTreeNode<T> min = findMin(root.customNeighbours[1], cd);
+                // replace root with the min point
+                root.setPoint(min.getPoint());
+                // recursively remove p
+                root.customNeighbours[1] = deleteRec(root.customNeighbours[1], min.getPoint(), depth + 1);
+            } else if (root.customNeighbours[0] != null) {
+                CustomKDTreeNode<T> min = findMin(root.customNeighbours[0], cd);
+                root.setPoint(min.getPoint());
+                root.customNeighbours[0] = deleteRec(root.customNeighbours[0], min.getPoint(), depth + 1);
+            } else {
+                // if deleting leaf node, just do
+                return null;
+            }
+            return root;
+        }
+        if (point[cd] < root.getPoint()[cd])
+            root.customNeighbours[0] = deleteRec(root.customNeighbours[0], point, depth+1);
+        else
+            root.customNeighbours[1] = deleteRec(root.customNeighbours[1], point, depth+1);
+        return root;
+    }
+
+    public CustomKDTreeNode<T> search(double[] point) {
+        return searchRec(root, point, 0);
+    }
+    private CustomKDTreeNode<T> searchRec(CustomKDTreeNode<T> root, double[] point, int depth) {
+        int cd = depth % dimensions;
+        if(root == null || Arrays.equals(root.getPoint(), point)) return root;
+        if(root.getPoint()[cd] > point[cd]) return searchRec(root.customNeighbours[0],point,depth+1);
+        else return searchRec(root.customNeighbours[1],point,depth+1);
+    }
 
     public double getDistanceSquared(double[] point1, double[] point2) {
         double distance = 0;
